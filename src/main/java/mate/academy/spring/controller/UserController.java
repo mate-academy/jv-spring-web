0555bf882c1d0d4c672c1ca2bf678728d671f4cf
safordog <safordog@gmail.com>
2021-06-14 20:52:25 +0300

    changed annotations

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,13 +6,12 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserMapper;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
     private UserService userService;
@@ -23,13 +22,11 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
-    @ResponseBody
     @GetMapping("/{userId}")
     public UserResponseDto get(@PathVariable Long userId) {
         return userMapper.parse(userService.get(userId));
     }
 
-    @ResponseBody
     @GetMapping
     public List<UserResponseDto> getAll() {
         return userService.getAll().stream()
@@ -37,7 +34,6 @@ public class UserController {
                 .collect(Collectors.toList());
     }
 
-    @ResponseBody
     @GetMapping("/inject")
     public String injectMockData() {
         User bob = new User("Bob", "Bobchenko");
