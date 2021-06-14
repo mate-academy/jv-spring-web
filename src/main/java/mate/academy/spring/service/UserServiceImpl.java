d3ba4d16f4db2ae8810a8478399d936a1a9f1d62
mankokolya <mankokolya@gmail.com>
2021-06-14 14:23:33 +0300

    renamed listUsers to getAllUsers

--- src/main/java/mate/academy/spring/service/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -19,7 +19,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<User> listUsers() {
+    public List<User> getAllUsers() {
         return userDao.getAll();
     }
 
