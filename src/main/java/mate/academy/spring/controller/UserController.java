906fc7a10240e8084dd9ec77e7022f1883362789
Dalai <semenov.dalay@gmail.com>
2021-06-16 11:08:06 +0300

    added solution

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,47 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    private UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        User dalai = new User("Dalai", "Semenov");
+        userService.add(dalai);
+        User mike = new User("Mike", "Novitskiy");
+        userService.add(mike);
+        User karina = new User("Karina", "Shamova");
+        userService.add(karina);
+        return "Data in the database!";
+    }
+}
