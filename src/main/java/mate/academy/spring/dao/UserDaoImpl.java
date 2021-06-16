7012c57758f104ab8fa9ea73eb17c1118f8dd343
Vlad-Andr <andreev.v99@gmail.com>
2021-06-16 08:56:33 +0300

    fixed catch block

--- src/main/java/mate/academy/spring/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -52,7 +52,7 @@ public class UserDaoImpl implements UserDao {
         try (Session session = sessionFactory.openSession()) {
             return Optional.ofNullable(session.get(User.class, id));
         } catch (Exception e) {
-            throw new RuntimeException("User not found", e);
+            throw new RuntimeException("User not found with id: " + id, e);
         }
     }
 }
