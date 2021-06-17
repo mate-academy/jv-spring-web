44999a9619983944416c31dc67c3bbc9125fb2c5
SerKisJD <sergej.kislyakov@gmail.com>
2021-06-17 13:13:51 +0300

    annotation instead constructor added

--- src/main/java/mate/academy/spring/model/dto/UserResponseDto.java
+++ src/main/java/mate/academy/spring/model/dto/UserResponseDto.java
@@ -1,16 +1,12 @@
 package mate.academy.spring.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String name;
     private String lastName;
-
-    public UserResponseDto(Long id, String name, String lastName) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-    }
 }
