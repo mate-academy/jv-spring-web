ff481cfb64b47f74e3f9b376a5df6a0d00ca2a72
Darina <to.darina.to>
2021-06-17 08:03:17 +0300

    student review1 done

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,13 +6,12 @@ import mate.academy.spring.dto.UserResponseDto;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserDtoMapper;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
@@ -24,7 +23,6 @@ public class UserController {
         this.userDtoMapper = userDtoMapper;
     }
 
-    @ResponseBody
     @GetMapping("/inject")
     public String create() {
         User bob = new User("Bob", "R");
@@ -36,13 +34,11 @@ public class UserController {
         return "Done";
     }
 
-    @ResponseBody
     @GetMapping("/{userId}")
     public UserResponseDto get(@PathVariable Long userId) {
         return userDtoMapper.parse(userService.getById(userId));
     }
 
-    @ResponseBody
     @GetMapping
     public List<UserResponseDto> getAll() {
         return userService.listUsers().stream()
