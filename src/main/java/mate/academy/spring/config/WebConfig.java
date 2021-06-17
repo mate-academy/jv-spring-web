ba17d287033b40a997f997b4632e4efcd27efbc2
oleksandr-shulha <shulgalex@gmail.com>
2021-06-17 23:03:42 +0300

    first

--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -7,5 +7,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "mate.academy.spring.controller")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 }
