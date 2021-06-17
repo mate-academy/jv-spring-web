8d0b4eb108648bd7cc6f1036512c6d8c828ad756
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-06-17 17:32:37 +0300

    testing

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,62 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService,
+                          UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto getById(@PathVariable Long userId) {
+        return userDtoMapper.parseToDto(userService.get(userId));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.listUsers().stream()
+                .map(userDtoMapper::parseToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String injectTestData() {
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setLastName("Bobson");
+
+        User alice = new User();
+        alice.setName("Alice");
+        alice.setLastName("Alison");
+
+        User jack = new User();
+        jack.setName("Jack");
+        jack.setLastName("Jackson");
+
+        userService.add(bob);
+        userService.add(alice);
+        userService.add(jack);
+        return "All users were saved to DB!";
+    }
+}
