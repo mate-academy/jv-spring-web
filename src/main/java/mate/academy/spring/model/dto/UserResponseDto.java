207b2c6aded50e6f9f261fc8f2b4a88558575dc0
DeStinyShell <dmitriy4ever97@gmail.com>
2021-06-18 12:31:22 +0300

    Done Homework.

---
+++ src/main/java/mate/academy/spring/model/dto/UserResponseDto.java
@@ -0,0 +1,11 @@
+package mate.academy.spring.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String lastName;
+
+}
