e42fd9365d9e2fa8adcec246d236d3a7e6ee2e59
Gord-Nik <75978452+Gord-Nik@users.noreply.github.com>
2021-06-18 11:48:37 +0300

    new changes

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,11 +6,13 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserMapper;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
     private UserService userService;
     private UserMapper userMapper;
@@ -36,7 +38,7 @@ public class UserController {
         return userMapper.parse(userService.getById(userId));
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<UserResponseDto> getAll() {
         return userService.getAll().stream()
                 .map(userMapper::parse)
