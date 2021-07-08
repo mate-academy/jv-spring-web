1de0afc147609825456a219ede57e18b9796c4ae
rkrylov <Cate77dila>
2021-07-08 15:50:34 +0300

    refactored

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,17 +1,16 @@
 package mate.academy.spring.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
-import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/mate/academy/spring/service/UserMapper.java
+++ src/main/java/mate/academy/spring/service/UserMapper.java
@@ -5,7 +5,8 @@ import mate.academy.spring.model.dto.UserResponseDto;
 
 public class UserMapper {
 
-    private UserMapper() {}
+    private UserMapper() {
+    }
 
     public static UserResponseDto parse(User user) {
         return new UserResponseDto(user.getId(), user.getName(), user.getLastName());
