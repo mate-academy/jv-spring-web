43e6cee948ee020c3d6d68a0187db16852006236
Daniel Kuruch <kuruch1004@gmail.com>
2021-08-18 00:15:59 +0300

    implemented UserController, UserResponseDto, UserDtoMapper,added WebAppInitializer, WebConfig

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,46 @@
+package mate.academy.spring.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/users/")
+    public List<UserResponseDto> getUsers() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserResponseDto getUserById(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/users/inject")
+    public String injectMockData() {
+        User[] mySiblings = {
+                new User("Andrew", "Kuruch"),
+                new User("Tanya", "Kasap"),
+                new User("Irina", "Gordt")
+        };
+        Arrays.stream(mySiblings).forEach(userService::add);
+        return "Test users have been saved";
+    }
+}
