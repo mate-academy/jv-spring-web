d683fa81e1c8f062d3fbd7ac3d863ac6730ca622
Daniel Kuruch <kuruch1004@gmail.com>
2021-08-18 13:20:07 +0300

    refactored annotatinos

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
@@ -23,7 +23,7 @@ public class UserController {
         this.userDtoMapper = userDtoMapper;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public List<UserResponseDto> getUsers() {
         return userService.getAll().stream()
                 .map(userDtoMapper::parse)
