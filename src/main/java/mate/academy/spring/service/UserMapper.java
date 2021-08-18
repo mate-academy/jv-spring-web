0e312ec75ea0f35ba1331b2f87eb931d75a99c90
Tamara-Paskalova <forptv@gmail.com>
2021-08-18 16:28:01 +0300

    implementer UserController, UserMapper, UserResponseDto, WebAppInitializer, WebConfig

---
+++ src/main/java/mate/academy/spring/service/UserMapper.java
@@ -0,0 +1,16 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDto parse(User user) {
+        UserResponseDto dto = new UserResponseDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setLastName(user.getLastName());
+        return dto;
+    }
+}
