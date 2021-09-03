c1f0f71850309d7b1e8b1255ea71c528756cb8d0
Dana Khromenko <danadiadius@gmail.com>
2021-09-03 16:02:08 +0300

    - added @RequestMapping and @RestController("/users")

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,11 +6,13 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserMapper;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RequestMapping
+@RestController("/users")
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
