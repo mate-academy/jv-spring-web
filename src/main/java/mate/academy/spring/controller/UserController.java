713f477435e82f551af2da051e6557d5944eed10
Andrii Demchenko <avd998ne@gmail.com>
2021-11-06 19:33:27 +0200

    added UserResponseDto UserDtoMapper and get method in UserController

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,12 +1,39 @@
 package mate.academy.spring.controller;
 
-import org.springframework.stereotype.Controller;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService,
+                          UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAllUsers() {
+        return userService.getAll()
+                .stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    public UserResponseDto get(@PathVariable Long id) {
+        return userDtoMapper.parse(userService.get(id));
     }
 }
