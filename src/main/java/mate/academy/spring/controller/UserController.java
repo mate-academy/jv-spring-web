801e16af4038e0abb003d9ab8d5b7e9340487e87
Maxim Moshenskiy <osidiv@ukr.net>
2021-11-08 21:14:58 +0200

    fixed bugs

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,7 +6,9 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,21 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
 
-    public UserController(UserService userService) {
+    @Autowired
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
         this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
     }
 
     @GetMapping("/{userId}")
-    public UserResponseDto get(Long userId) {
-        return UserDtoMapper.parse(userService.get(userId));
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
     }
 
     @GetMapping
     public List<UserResponseDto> getAll() {
         return userService.getAll()
                 .stream()
-                .map(UserDtoMapper::parse)
+                .map(userDtoMapper::parse)
                 .collect(Collectors.toList());
     }
 
