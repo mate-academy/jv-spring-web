b0219ef693d81ee3b1ce8a6fcb9a2c1dae7ecdb4
SolnalarmJulia <solnalarm@meta.ua>
2021-11-08 14:11:17 +0200

    UserController

---
+++ src/main/java/mate/academy/spring/service/mapper/UserDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserRequestDto;
+import mate.academy.spring.model.dto.UserResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoMapper {
+    public UserResponseDto parse(User user) {
+        UserResponseDto responseDto = new UserResponseDto();
+        responseDto.setId(user.getId());
+        responseDto.setName(user.getName());
+        responseDto.setLastName(user.getLastName());
+        return responseDto;
+    }
+
+    public User toModel(UserRequestDto requestDto) {
+        User user = new User();
+        user.setName(requestDto.getName());
+        user.setLastName(requestDto.getLastName());
+        return user;
+    }
+}
