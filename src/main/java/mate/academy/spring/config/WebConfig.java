0ee74e9a1e48589ef3c6b087a472466aad22e3b5
SolnalarmJulia <solnalarm@meta.ua>
2021-11-08 14:28:38 +0200

    WebConfig refactoring

--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -11,12 +11,4 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan(basePackages = "mate.academy.spring.controller")
 public class WebConfig {
-    @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
