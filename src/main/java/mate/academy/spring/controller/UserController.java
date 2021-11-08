0be2f2d0e0b431e857e459071e3e22a77c1f0074
Maxim Moshenskiy <osidiv@ukr.net>
2021-11-08 18:54:47 +0200

    fixed bugs

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -6,10 +6,13 @@ import mate.academy.spring.model.User;
 import mate.academy.spring.model.dto.UserResponseDto;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserDtoMapper;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
