aad456d9da1079f558403b5e3f228a3640c85bb0
Serhii Blakh <s.a.blakh@ukr.net>
2021-11-09 11:43:54 +0200

    configured Spring and DB, created UserResponseDto, implemented UserController

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,58 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/")
+    public List<UserResponseDto> getAllUser() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        User samurai = new User();
+        samurai.setName("Samurai");
+        samurai.setLastName("Carpenter");
+
+        User pask = new User();
+        pask.setName("Pask");
+        pask.setLastName("Maers");
+
+        User nick = new User();
+        nick.setName("Nick");
+        nick.setLastName("Samory");
+
+        userService.add(samurai);
+        userService.add(pask);
+        userService.add(nick);
+        return "Done!";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserResponseDto getUserById(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
+    }
+}
