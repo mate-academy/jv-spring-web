eb19c68e6caa23baba6c8badd4e8f95d7de69088
Stas Shepet <milka33349@gmail.com>
2021-11-09 14:14:30 +0200

    spring web was implemented

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,49 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    private UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/inject")
+    public boolean injectData() {
+        try {
+            userService.add(new User("Hermione", "Granger"));
+            userService.add(new User("Ron", "Weasley"));
+            userService.add(new User("Harry", "Potter"));
+            userService.add(new User("Cedric", "Diggory"));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parseToUserResponseDto(userService.get(userId));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userMapper::parseToUserResponseDto)
+                .collect(Collectors.toList());
+    }
+}
