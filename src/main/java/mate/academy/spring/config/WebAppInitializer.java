63c46804f5594b9ae373d1a1185a641f9193578e
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-11-09 14:39:30 +0200

    removed rendudant dependecies and renamed classes

--- src/main/java/mate/academy/spring/config/WebAppInitializer.java
+++ src/main/java/mate/academy/spring/config/WebAppInitializer.java
@@ -1,12 +1,20 @@
 package mate.academy.spring.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-@EnableWebMvc
-@Configuration
-@ComponentScan(basePackages = "mate.academy.spring.controller")
-public class WebAppInitializer implements WebMvcConfigurer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -1,21 +1,11 @@
 package mate.academy.spring.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-public class WebConfig
-        extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {AppConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {WebConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"/"};
-    }
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "mate.academy.spring.controller")
+public class WebConfig {
 }
