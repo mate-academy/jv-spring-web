d209d7571149a4cc8c3f34c11d3065a4e825de91
V <v.ivashchenko42@protonmail.com>
2021-11-09 15:03:53 +0300

    created UserResponseDto, implemented UserController and created UserMapper

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,51 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAllUsers() {
+        return userService.getAll().stream()
+                .map(userMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        User userBob = new User("Bob", "Bob");
+        userService.add(userBob);
+        User userAlice = new User("Alice", "Alice");
+        userService.add(userAlice);
+        User userJack = new User("Jack", "Smith");
+        userService.add(userJack);
+        User userTom = new User("Tom", "Taylor");
+        userService.add(userTom);
+        return "Users were added!";
+    }
+}
