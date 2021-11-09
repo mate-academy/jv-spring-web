62aef01007d0a4be5a2739e126747bc7a28967ad
Serhii Blakh <s.a.blakh@ukr.net>
2021-11-09 15:27:33 +0200

    fixed checkstyle

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -9,7 +9,6 @@ import mate.academy.spring.service.mapper.UserDtoMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
