adb622a53cb084dbf1fde62fa357db09a575d8bc
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-11-09 12:37:04 +0200

    The first solution.

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,48 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService,
+                          UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/users/inject")
+    public String addUser() {
+        User bob = new User(1L, "Bob", "Boon");
+        User alice = new User(2L, "Alice", "Alicia");
+        User max = new User(3L, "Max", "Maximovich");
+        userService.add(bob);
+        userService.add(alice);
+        userService.add(max);
+        return "Users add to the DB";
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/users/") //
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
