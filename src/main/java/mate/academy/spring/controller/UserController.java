ae06b411760525954668c81c7f07f3c60d29a485
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-12-31 11:39:09 +0200

    tested and veryfied with checklist

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,50 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/inject")
+    public String injectData() {
+        User user1 = new User("Evgeniy", "Onegin");
+        userService.add(user1);
+        User user2 = new User("Vladimir", "Lenskiy");
+        userService.add(user2);
+        User user3 = new User("Tatyana", "Larina");
+        userService.add(user3);
+        return "Users were succesfully saved to DB";
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll()
+                .stream()
+                .map(userMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
