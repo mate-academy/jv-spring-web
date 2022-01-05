e9c4c5a7a4a3654601f8b869979015bccbd71255
Andriy-dz <andriydzdz@gmail.com>
2022-01-05 15:14:31 +0200

    implemented the first web program

---
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDto parse(User user) {
+        return new UserResponseDto(user.getId(),
+                user.getName(), user.getLastName());
+    }
+}
