369ef48ba26193317a81db456dee03fa89a9e20d
Oleg Shiriaev <mystidreamer@gmail.com>
2022-01-11 00:29:59 +0200

    spring web was implemented

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,53 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        User valera = new User();
+        valera.setName("Valera");
+        valera.setLastName("Petrov");
+        User egorka = new User();
+        egorka.setName("Egorka");
+        egorka.setLastName("Ivanov");
+        User mikhael = new User();
+        mikhael.setName("Michael");
+        mikhael.setLastName("Shumakher");
+        userService.add(valera);
+        userService.add(egorka);
+        userService.add(mikhael);
+        return "Done!";
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/")
+    public List<UserResponseDto> getAllUsers() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
