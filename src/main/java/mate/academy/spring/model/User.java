e3c5d16704ce6ec4224f3d9cd6361d3fe2bb3898
Roman Polulikh <polilikh.r@gmail.com>
2022-01-10 13:08:52 +0200

    implemented all required classes

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,16 +1,20 @@
 package mate.academy.spring.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(name = "last_name")
     private String lastName;
 
     public Long getId() {
