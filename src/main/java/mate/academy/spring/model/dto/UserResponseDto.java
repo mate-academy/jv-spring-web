5058395261b5b2703b1aa86867c8abbb03126360
Anatolii Huryk <tolik.mario96@gmail.com>
2022-01-11 11:57:34 +0200

    create Spring web

---
+++ src/main/java/mate/academy/spring/model/dto/UserResponseDto.java
@@ -0,0 +1,40 @@
+package mate.academy.spring.model.dto;
+
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponseDto{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", lastName='" + lastName + '\''
+                + '}';
+    }
+}
