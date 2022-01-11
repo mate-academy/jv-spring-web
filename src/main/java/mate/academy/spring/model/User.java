085bffb19da5d046a14bea5f3c0d7cf415e3d1f0
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2022-01-11 13:14:38 +0200

    All requested fixes done

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -17,6 +17,9 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    public User() {
+    }
+
     public User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
