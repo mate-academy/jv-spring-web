7ff3fb98744e6d9b388c56995fdf4ee82eff209b
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-11 11:32:26 +0200

    was implemented UserController, UserResponseDto

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,45 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserMapper userMapper;
+    private UserService userService;
+
+    public UserController(UserMapper userMapper, UserService userService) {
+        this.userMapper = userMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto getById(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String injectMockUsers() {
+        userService.add(new User("username1", "lastname1"));
+        userService.add(new User("username2", "lastname2"));
+        userService.add(new User("username3", "lastname3"));
+        userService.add(new User("username4", "lastname4"));
+        return "Mock data was injected !";
+    }
+}
---
+++ src/main/java/mate/academy/spring/dto/UserResponseDto.java
@@ -0,0 +1,39 @@
+package mate.academy.spring.dto;
+
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String lastName;
+
+    public UserResponseDto(){
+    }
+
+    public UserResponseDto(Long id, String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
