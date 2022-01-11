260ca345898e82a4c2fad0ab7ba96e6236fbcd0a
Artik <Artik25942@gmail.com>
2022-01-11 15:58:19 +0200

    after mr

--- src/main/java/mate/academy/spring/service/mapper/UserDtoMapper.java
+++ src/main/java/mate/academy/spring/service/mapper/UserDtoMapper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserDtoMapper {
-
     public UserResponseDto parse(User user) {
         UserResponseDto responseDto = new UserResponseDto();
         responseDto.setId(user.getId());
