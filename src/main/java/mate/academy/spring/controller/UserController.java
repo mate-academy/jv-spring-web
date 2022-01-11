a48e171537771815eeabe1b85b28a14ee1f5a79b
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2022-01-11 13:27:38 +0200

    Project created

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,53 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        User john = new User();
+        john.setName("John");
+        john.setLastName("Johnson");
+        User alice = new User();
+        alice.setName("Alice");
+        alice.setLastName("McCartur");
+        User bruce = new User();
+        bruce.setName("Bruce");
+        bruce.setLastName("Willis");
+        userService.add(john);
+        userService.add(alice);
+        userService.add(bruce);
+        return "Users created!";
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto get(@PathVariable Long id) {
+        return userDtoMapper.parse(userService.get(id));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
