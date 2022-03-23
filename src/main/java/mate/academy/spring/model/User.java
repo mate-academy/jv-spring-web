8621995a5359d7ea749837ab1bdb0c89609c73df
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-23 14:12:09 +0200

    code was refactored

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -8,11 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 @RequiredArgsConstructor
 @NoArgsConstructor
