30652f6fd82ef3a017f29a0b5897f3b15b544cf8
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2022-05-24 16:38:03 +0300

    Solution of the task

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,46 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/inject")
+    public String inject() {
+        userService.add(new User("John","Doe"));
+        userService.add(new User("Emily","Stone"));
+        userService.add(new User("Hugh","Dane"));
+        return "Users are injected!";
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto get(@PathVariable Long id) {
+        return userMapper.parse(userService.get(id));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll()
+                .stream().map(userMapper::parse)
+                .collect(Collectors.toList());
+    }
+}
