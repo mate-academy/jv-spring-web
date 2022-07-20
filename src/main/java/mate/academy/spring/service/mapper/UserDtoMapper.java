cda9a9824dc034a3f8e08b5dcfa008f9ab8fcd74
Lyudmila D <dunda94lyudmila@gmail.com>
2022-07-20 10:32:12 +0300

    controller created

---
+++ src/main/java/mate/academy/spring/service/mapper/UserDtoMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoMapper {
+
+    public UserResponseDto parse(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        return userResponseDto;
+    }
+
+    public User toModel(UserResponseDto userResponseDto) {
+        User user = new User();
+        user.setId(userResponseDto.getId());
+        user.setFirstName(userResponseDto.getFirstName());
+        user.setLastName(userResponseDto.getLastName());
+        return user;
+    }
+}
