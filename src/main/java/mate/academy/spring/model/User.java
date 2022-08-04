0dc814ed2dd3299638640b1d861f7e72c791c3fa
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-08-04 21:51:52 +0300

    task done

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -12,7 +12,15 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
-
+    
+    public User() {
+    }
+    
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
     public Long getId() {
         return id;
     }
