8c58a917bcbe77dfc2cbda977fe65c041c9f7827
drapail <mv.dibrov@gmail.com>
2022-08-20 18:12:41 +0300

    db and spring-web configured

---
+++ src/main/java/mate/academy/spring/Main.java
@@ -0,0 +1,15 @@
+package mate.academy.spring;
+
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.dao.UserDaoImpl;
+import mate.academy.spring.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
