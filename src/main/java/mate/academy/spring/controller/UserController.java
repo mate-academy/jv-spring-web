f6cd05931ec6c146dade27f00901ee53158ad846
Nazar <litvik.12@gmail.com>
2022-09-28 22:55:14 +0300

    tests passed

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,54 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final UserMapper mapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper mapper) {
+        this.userService = userService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/users/inject")
+    public String injectMockData() {
+        User john = new User();
+        john.setFirstName("John");
+        john.setLastName("Doe");
+        User emily = new User();
+        emily.setFirstName("Emily");
+        emily.setLastName("Stone");
+        User hugh = new User();
+        hugh.setFirstName("Hugh");
+        hugh.setLastName("Dane");
+
+        userService.add(john);
+        userService.add(emily);
+        userService.add(hugh);
+        return "Users are injected!";
+    }
+
+    @GetMapping("/users")
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream().map(mapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return mapper.parse(userService.get(userId));
+    }
+
+}
