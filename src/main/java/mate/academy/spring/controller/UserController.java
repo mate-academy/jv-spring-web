2c4b9ee94ea8a8323f9b28025b7411cf0e68d35c
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-28 19:32:25 +0300

    UserMapper is replaced into service

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.spring.dto.UserResponseDto;
 import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserMapper;
 import mate.academy.spring.service.UserService;
-import mate.academy.spring.service.mapper.UserDtoMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,22 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final UserDtoMapper userDtoMapper;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
-        this.userDtoMapper = userDtoMapper;
+        this.userMapper = userMapper;
     }
 
     @GetMapping("/{userId}")
-    public UserResponseDto get(Long userId) {
-        return userDtoMapper.parse(userService.get(userId));
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
     }
 
     @GetMapping("/")
     public List<UserResponseDto> getAll() {
         return userService.getAll().stream()
-                .map(userDtoMapper::parse)
+                .map(userMapper::parse)
                 .collect(Collectors.toList());
     }
 
