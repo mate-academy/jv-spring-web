5bb8d6dff53de79855b32bfed4e708545a6a9935
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-15 00:09:27 +0200

    implement methods in UserController

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,21 +1,30 @@
 package mate.academy.spring.controller;
 
 import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.mapper.UserDtoMapper;
 import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping//проверка без литерала тоже будет работать или нет
 public class UserController {
-    private UserDao userDao;
+    private UserService userService;
+    private UserDtoMapper dtoMapper;
 
     @Autowired
-    public UserController(UserDao userDao) {
-        this.userDao = userDao;
+    public UserController(UserService userService,
+                          UserDtoMapper dtoMapper) {
+        this.userService = userService;
+        this.dtoMapper = dtoMapper;
     }
 
     @GetMapping("/users/inject")
@@ -30,14 +39,21 @@ public class UserController {
         hugh.setFirstName("Hugh");
         hugh.setLastName("Dane");
 
-        userDao.add(john);
-        userDao.add(emily);
-        userDao.add(hugh);
+        userService.add(john);
+        userService.add(emily);
+        userService.add(hugh);
     }
 
     @GetMapping("/users/{userId}")
     public UserResponseDto getUserById(@PathVariable Long id) {
+        return dtoMapper.getDto(userService.get(id));
+    }
 
+    @GetMapping("/users")
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(dtoMapper::getDto)
+                .collect(Collectors.toList());
     }
 
 
