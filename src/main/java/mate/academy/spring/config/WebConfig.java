1a0f1c3c61f2412a26438412424d9b40850f8fd6
asha <ashajanebrown@gmail.com>
2022-10-18 23:38:35 +0300

    added annotation @EnableWebMvc

--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "mate.academy.spring.controller")
 public class WebConfig implements WebMvcConfigurer {
