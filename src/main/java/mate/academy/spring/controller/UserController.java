d643786f7045d83a938e37c1b4b72f3b22c4b14e
Alina Nikiforova <0500302717a@gmail.com>
2022-10-28 11:45:21 +0200

    added

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,49 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userDtoMapper;
+
+    public UserController(UserService userService,
+                          UserMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll()
+                .stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto get(@PathVariable Long id) {
+        return userDtoMapper.parse(userService.get(id));
+    }
+
+    @GetMapping("/inject")
+    public String inject() {
+        User john = new User("John", "Doe");
+        userService.add(john);
+        User emily = new User("Emily", "Stone");
+        userService.add(emily);
+        User hugh = new User("Hugh", "Dane");
+        userService.add(hugh);
+        return "Users are injected!";
+    }
+}
