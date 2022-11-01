31f0fe1d9d2b8eae608776afb1da502ea0f6b139
Igor Petrichenko <i.petrichenko@gmail.com>
2022-11-01 13:23:52 +0200

    HW Spring web

---
+++ src/main/java/mate/academy/spring/model/dto/UserResponseDto.java
@@ -0,0 +1,41 @@
+package mate.academy.spring.model.dto;
+
+public class UserResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("UserResponseDto{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
