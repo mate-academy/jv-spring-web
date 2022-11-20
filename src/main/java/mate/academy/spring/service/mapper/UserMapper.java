6aa17996e19a29941aadcbac68ea5e751ab69140
Oleg Pruh <olegpruh1@gmail.com>
2022-11-20 09:26:10 +0000

    Task completed

---
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDto parse(User user) {
+        UserResponseDto responseDto = new UserResponseDto();
+        responseDto.setId(user.getId());
+        responseDto.setFirstName(user.getFirstName());
+        responseDto.setLastName(user.getLastName());
+        return responseDto;
+    }
+
+    public User toModel(UserResponseDto responseDto) {
+        User user = new User();
+        user.setId(responseDto.getId());
+        user.setFirstName(responseDto.getFirstName());
+        user.setLastName(responseDto.getLastName());
+        return user;
+    }
+}
