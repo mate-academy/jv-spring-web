d1ff14420d869dadb018e764a569caf5d81a1105
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-12-12 14:52:21 +0200

    hw done

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -2,8 +2,8 @@ package mate.academy.spring.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import mate.academy.spring.model.User;
 import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
 import mate.academy.spring.service.mapper.UserMapper;
 import org.springframework.web.bind.annotation.GetMapping;
