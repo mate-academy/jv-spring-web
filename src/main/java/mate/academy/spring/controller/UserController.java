62eb2b36f1d30cdebc4cf192b5ea7c2dfcf28cc4
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-01-12 14:33:23 +0200

    Correct code styles

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -3,18 +3,14 @@ package mate.academy.spring.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.dto.UserResponseDto;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserMapper;
-import mate.academy.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,7 +29,8 @@ public class UserController {
     public UserResponseDto get(@PathVariable Long userId) {
         return userDao.get(userId)
                 .map(userMapper::mapFromEntityToDto)
-                .orElseThrow(() -> new NoSuchElementException("Can't find user with id " + userId + "."));
+                .orElseThrow(() -> new NoSuchElementException("Can't find user with id "
+                        + userId + "."));
     }
 
     @GetMapping("/users")
