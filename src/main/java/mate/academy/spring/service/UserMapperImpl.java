7534819d3a5281a76c55770adaead5bef491e567
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-26 21:15:47 +0200

    add UserMapperImpl

---
+++ src/main/java/mate/academy/spring/service/UserMapperImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapperImpl implements UserMapper {
+    @Override
+    public UserResponseDto toDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        return userResponseDto;
+    }
+}
