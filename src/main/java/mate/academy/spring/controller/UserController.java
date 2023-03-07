6f4b43d78310be7481f89ce042841278d00513f7
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-03-08 00:51:54 +0200

    feat: jv-spring-web #comment was implemented first REST requests

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,63 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    @Autowired
+    public UserController(
+            UserService userService,
+            UserDtoMapper userDtoMapper
+    ) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping
+    public List<UserResponseDto> users() {
+        return userService.getAll()
+                .stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String usersInject() {
+        User john = new User();
+        john.setFirstName("John");
+        john.setLastName("Doe");
+        userService.add(john);
+
+        User emily = new User();
+        emily.setFirstName("Emily");
+        emily.setLastName("Stone");
+        userService.add(emily);
+
+        User hugh = new User();
+        hugh.setFirstName("Hugh");
+        hugh.setLastName("Dane");
+        userService.add(hugh);
+
+        return "Users are injected!";
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto getById(@PathVariable String userId) {
+        User user = userService.get(Long.valueOf(userId));
+        return userDtoMapper.parse(user);
+    }
+}
