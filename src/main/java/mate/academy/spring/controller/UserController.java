962076c0af8243a9b425cacc78c61aaecf3cd307
Diana <dlomei283@gmail.com>
2023-03-14 22:34:22 +0200

    .

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -50,10 +49,8 @@ public class UserController {
         return userDtoMapper.parse(userService.get(userId));
     }
 
-    @ResponseBody
-    @GetMapping("/users/all")
+    @GetMapping("/users")
     public List<UserResponseDto> getAll() {
-        injectMockData();
         return userService.getAll().stream()
                 .map(userDtoMapper::parse)
                 .collect(Collectors.toList());
