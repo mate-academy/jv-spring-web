5e8e42e86618dc07b331ae45c3c180dbfffec307
Olexii Melnik <alexmelnikk@gmail.com>
2023-03-28 14:15:28 +0300

    f1st commit

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,60 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.model.User;
+import mate.academy.spring.model.dto.UserResponseDto;
+import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService,
+                          UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/users/inject")
+    public String injectMocData() {
+        User userJ = new User();
+        userJ.setFirstName("John");
+        userJ.setLastName("Doe");
+
+        User userE = new User();
+        userE.setFirstName("Emily");
+        userE.setLastName("Stone");
+
+        User userH = new User();
+        userH.setFirstName("Hugh");
+        userH.setLastName("Dane");
+
+        userService.add(userJ);
+        userService.add(userE);
+        userService.add(userH);
+
+        return "Done!";
+    }
+
+    @GetMapping("/users{userId}")
+    public UserResponseDto get(Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/users")
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+             .map(userMapper::parse)
+             .collect(Collectors.toList());
+    }
+
+}
