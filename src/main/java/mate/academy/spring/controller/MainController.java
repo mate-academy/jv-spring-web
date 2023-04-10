6b763436fabfcd999cbc033fbbf057ed052a28bd
Sergiy Golubchenko <galuba1@gmail.com>
2023-04-10 20:47:17 +0300

    User model was added. Add Dto. Add UserController

---
+++ src/main/java/mate/academy/spring/controller/MainController.java
@@ -0,0 +1,18 @@
+package mate.academy.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Slava Ukraini</h1>
+<jsp:include page="heder.jsp"></jsp:include>
+</body>
+</html>
