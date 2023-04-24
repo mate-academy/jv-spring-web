96dc99e6bae94da6754cd2e7f323059dce8c2709
Denys Dominskyi <dominskyj@gmail.com>
2023-04-24 23:45:40 +0300

    Initial commit

---
+++ src/main/java/mate/academy/spring/dto/UserResponseDto.java
@@ -0,0 +1,37 @@
+package mate.academy.spring.dto;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public UserResponseDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
