f8dc372947956709ed2e49d3c9df3b810bb8b156
Danilo Kozakov <45821252+AnonimToster@users.noreply.github.com>
2023-04-26 22:52:58 +0300

    solution

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,20 @@
+package mate.academy.spring.controller;
+
+import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+}
