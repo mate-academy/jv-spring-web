9c132a44e66e80daa8dc5c06cb3596b6f43a77f9
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-04-26 22:08:04 +0300

    created dto, cintroller

---
+++ src/main/java/mate/academy/spring/service/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.service.mapper;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDto parse(User user) {
+        UserResponseDto userResponseDto
+                = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        return userResponseDto;
+    }
+}
