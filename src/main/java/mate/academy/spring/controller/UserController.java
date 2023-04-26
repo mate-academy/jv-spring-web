388e64eccc24f29369544cda97e6becc7c89e5c1
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-04-26 03:05:37 +0300

    configs

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,12 @@
+package mate.academy.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+}
