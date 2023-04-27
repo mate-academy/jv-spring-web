6fab984ba481ad4db2857f41d2bfc099ee6a4b43
a.ponomarenko-at-201540164075 <NF1Y9pEm016SCxSUCNnwBwHKLQD+CU8N9IuhiFm/FsA=>
2023-04-27 15:55:24 +0300

    checkstyle fix

--- src/main/java/mate/academy/spring/config/WebConfig.java
+++ src/main/java/mate/academy/spring/config/WebConfig.java
@@ -1,11 +1,11 @@
-    package mate.academy.spring.config;
+package mate.academy.spring.config;
 
-    import org.springframework.context.annotation.ComponentScan;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-    @EnableWebMvc
-    @Configuration
-    @ComponentScan(basePackages = "mate.academy.spring.controller")
-    public class WebConfig {
-    }
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "mate.academy.spring.controller")
+public class WebConfig {
+}
