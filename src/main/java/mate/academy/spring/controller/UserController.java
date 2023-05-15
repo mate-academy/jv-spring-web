9103cb5f272ef5ee9c296aa96d041c38e5e05fff
dovy_al@ukr.net <dovy_al@ukr.net>
2023-05-15 21:01:47 +0300

    done

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,48 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserMapper;
+import mate.academy.spring.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("/users")
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream().map(userMapper::parse).toList();
+    }
+
+    @GetMapping("/users/inject")
+    public String inject() {
+        User firstUser = new User();
+        firstUser.setFirstName("John");
+        firstUser.setLastName("Doe");
+        userService.add(firstUser);
+        User secondUser = new User();
+        secondUser.setFirstName("Emily");
+        secondUser.setLastName("Stone");
+        userService.add(secondUser);
+        User thirdUser = new User();
+        thirdUser.setFirstName("Hugh");
+        thirdUser.setLastName("Dane");
+        userService.add(thirdUser);
+        return "Users are injected!";
+    }
+}
