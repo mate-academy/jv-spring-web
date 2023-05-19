f2277708b74cc9aec4431958b35d2e2334c70267
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-19 13:05:05 +0200

    Solved 1.1

--- src/main/java/mate/academy/spring/controller/UserController.java
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -1,12 +1,47 @@
 package mate.academy.spring.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    private final UserService userService;
+
+    private final UserDtoMapper userDtoMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping()
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parseUser)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userDtoMapper.parseUser(userService.get(userId));
+    }
+
+    @GetMapping("/inject")
+    public String inject() {
+        userService.add(new User("John", "Doe"));
+        userService.add(new User("Emily", "Stone"));
+        userService.add(new User("Hugh", "Dane"));
+        return "Users are injected!";
     }
 }
