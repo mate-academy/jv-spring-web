0f99e32f610eb85c1abe12099819d62398470193
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-06-18 12:32:37 +0200

    spring web task completed

---
+++ src/main/java/mate/academy/spring/service/UserMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapper {
+    public UserResponseDto parse(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        return userResponseDto;
+    }
+
+    public UserResponseDto toUserResponseDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        return userResponseDto;
+    }
+}
