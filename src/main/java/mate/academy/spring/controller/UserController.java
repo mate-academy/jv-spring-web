070a9f59ba9af5e6160d7c58c278cb17d03b0e91
Dmytro Katrevych <katrevichdv@gmail.com>
2023-07-02 13:46:19 +0300

    Spring MVC and DB were configured UserResponseDto, UserMapper and UserController were created

---
+++ src/main/java/mate/academy/spring/controller/UserController.java
@@ -0,0 +1,49 @@
+package mate.academy.spring.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.spring.dto.UserResponseDto;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import mate.academy.spring.service.mapper.UserDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    public UserController(UserService userService, UserDtoMapper userDtoMapper) {
+        this.userService = userService;
+        this.userDtoMapper = userDtoMapper;
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto get(@PathVariable Long userId) {
+        return userDtoMapper.parse(userService.get(userId));
+    }
+
+    @GetMapping("")
+    public List<UserResponseDto> getAll() {
+        return userService.getAll().stream()
+                .map(userDtoMapper::parse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inject")
+    public String inject() {
+        String[] fullNames = new String[] {"John Doe", "Emily Stone", "Hugh Dane"};
+        for (String fullName : fullNames) {
+            String[] names = fullName.split(" ");
+            User user = new User();
+            user.setFirstName(names[0]);
+            user.setLastName(names[1]);
+            userService.add(user);
+        }
+        return "Users are injected!";
+    }
+}
