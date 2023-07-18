7ef4e1e884f0096bce6287857cb4b47627d316e6
valdemar <vova.malyarchuk2@gmail.com>
2023-07-18 15:43:04 +0300

    Implemented task db

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -13,6 +13,14 @@ public class User {
     private String firstName;
     private String lastName;
 
+    public User() {
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public Long getId() {
         return id;
     }
